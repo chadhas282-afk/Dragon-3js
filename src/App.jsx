846d66c3643a45b1190a822b0c928e40a1227c2f
Sahil Chadha <Chadhas282@gmail.com>
2026-09-22 09:35:03 +0530

    Add requestAnimationFrame loop for Lenis smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -13,3 +13,8 @@ export default function App() {
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
