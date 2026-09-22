5db3c9ff4ee1f1ba5b072e8e19634a2918e53500
Sahil Chadha <Chadhas282@gmail.com>
2026-09-22 09:34:54 +0530

    Add Lenis smooth scrolling effect to App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,3 +8,8 @@ export default function App() {
   const cursorRef = useRef(null)
 
   useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    })
