afcfa82b2883949ceb32da2ec22ebc8d891d8633
Sahil Chadha <Chadhas282@gmail.com>
2026-09-23 13:20:09 +0530

    Fix missing newline at end of file in App component

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,8 @@ export default function App() {
     const moveCursor = (e) => {
       if (cursorRef.current) {
         cursorRef.current.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+        }
+    }
+    window.addEventListener('mousemove', moveCursor)
+
+    return () => {
