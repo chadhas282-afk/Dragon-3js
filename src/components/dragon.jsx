ec7e48c21fb5c359dcf8cc4866aab77f1b580f76
Sahil Chadha <Chadhas282@gmail.com>
2026-09-25 10:53:27 +0530

    Add NORMAL_MAP_PATH and MATCAP_PATHS constants to dragon.jsx

--- src/components/dragon.jsx
+++ src/components/dragon.jsx
@@ -33,3 +33,8 @@ gsap.registerPlugin(useGSAP)
 gsap.registerPlugin(ScrollTrigger)
 
 const MODEL_PATH = '/models/model.glb'
+const NORMAL_MAP_PATH = '/MI_M_B_44_Qishilong_body02_2_Inst_normal.png'
+
+const MATCAP_PATHS = [
+    '/matcap/mat-1.png', '/matcap/mat-2.png', '/matcap/mat-3.png', '/matcap/mat-4.png',
+    '/matcap/mat-5.png', '/matcap/mat-6.png', '/matcap/mat-7.png', '/matcap/mat-8.png',
